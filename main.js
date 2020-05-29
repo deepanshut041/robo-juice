35317f37a10e96fbdaff53f68cd176f5c6b681d5
Deepanshu Tyagi <deepanshut041@gmail.com>
2020-05-29 18:01:29 +0530

    Added python script runner and react to elctron

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const isDev = require('electron-is-dev'); 
 
-/*************************************************************
- * window management
- *************************************************************/
 
 let mainWindow = null
 
 const createWindow = () => {
-    mainWindow = new BrowserWindow({ width: 800, height: 600 })
-    mainWindow.loadURL(require('url').format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
 
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    })
+    mainWindow = new BrowserWindow({ width: 800, height: 600, fullscreen: !isDev})
+    mainWindow.removeMenu()
+
+    const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, './front-end/build/index.html')}`;
+
+    mainWindow.loadURL(startURL)
+
+    var python = require('child_process').spawn('python', ['./hello.py']);
+    python.stdout.on('data',function(data){
+        console.log("data: ",data.toString('utf8'));
+    });
 }
 
 app.on('ready', createWindow)
@@ -27,11 +25,5 @@ app.on('ready', createWindow)
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
-    }
-})
-
-app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow()
     }
 })
